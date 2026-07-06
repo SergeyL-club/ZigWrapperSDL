3cf6b5f8901be9ea3edc2b670de470f57afe9346
SergeyL-club <damnazzel@gmail.com>
2026-07-06 15:52:04 +0500

    fix c import

--- build.zig
+++ build.zig
@@ -6,18 +6,32 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     const sdl_windows_binary = b.dependency("sdl_windows_binary", .{});
+
+    const sdl_path_artifacts = switch (target.result.os.tag) {
+        .windows => switch (target.result.cpu.arch) {
+            .x86 => sdl_windows_binary.path("lib/x86"),
+            .x86_64 => sdl_windows_binary.path("lib/x64"),
+            .aarch64 => sdl_windows_binary.path("lib/arm64"),
+            else => @panic("[WrapperSDL] Not support target arch"),
+        },
+        else => @panic("[WrapperSDL] Not support target os"),
+    };
+
     const sdl_h_c = b.addTranslateC(.{
         .root_source_file = sdl_windows_binary.path("include/SDL.h"),
         .target = target,
         .optimize = optimize,
         .link_libc = true,
     });
+    sdl_h_c.addIncludePath(sdl_windows_binary.path("include"));
+
     const sdl_vulkan = b.addTranslateC(.{
         .root_source_file = sdl_windows_binary.path("include/SDL_vulkan.h"),
         .target = target,
         .optimize = optimize,
         .link_libc = true,
     });
+    sdl_vulkan.addIncludePath(sdl_windows_binary.path("include"));
 
     const mod = b.addModule("WrapperSDL", .{
         .root_source_file = b.path("src/root.zig"),
@@ -30,28 +44,16 @@ pub fn build(b: *std.Build) !void {
         .link_libc = true,
     });
 
-    const sdl_path_artifacts = switch (builtin.target.os.tag) {
-        .windows => switch (builtin.target.cpu.arch) {
-            .x86 => sdl_windows_binary.path("lib/x86"),
-            .x86_64 => sdl_windows_binary.path("lib/x64"),
-            .aarch64 => sdl_windows_binary.path("lib/arm64"),
-            else => @panic("[WrapperSDL] Not support target arch"),
-        },
-        else => @panic("[WrapperSDL] Not support target os"),
-    };
-
-    const affix = switch (builtin.target.os.tag) {
-        .windows => ".dll",
-        else => @panic("[WrapperSDL] Not support target os"),
-    };
-
-    const install_filename = try std.fmt.allocPrint(b.allocator, "SDL3.{s}", .{affix});
-    errdefer @panic("Error alloc string");
-    defer b.allocator.free(install_filename);
-
-    const install_file = b.addInstallFileWithDir(sdl_path_artifacts, .bin, install_filename);
-    b.getInstallStep().dependOn(&install_file.step);
-
     mod.addLibraryPath(sdl_path_artifacts);
     mod.linkSystemLibrary("SDL3", .{});
+
+    const affix = switch (builtin.target.os.tag) {
+        .windows => "dll",
+        else => @panic("[WrapperSDL] Not support target os"),
+    };
+
+    const install_filename = b.fmt("SDL3.{s}", .{affix});
+
+    const install_file = b.addInstallFileWithDir(sdl_path_artifacts.path(install_filename), .bin, install_filename);
+    b.getInstallStep().dependOn(&install_file.step);
 }
